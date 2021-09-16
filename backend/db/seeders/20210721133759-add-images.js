900090168445008c861defe6bb04b08f8e122acb
Mark Vasquez <markjasonvasquez1@gmail.com>
2021-09-16 08:59:30 -0700

    Add seeds

--- backend/db/seeders/20210721133759-add-images.js
+++ backend/db/seeders/20210721133759-add-images.js
@@ -25,6 +25,186 @@ module.exports = {
 					imageURL:
 						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
 				},
+				{
+					businessId: 2,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/wip.png",
+				},
+				{
+					businessId: 2,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/wip2.png",
+				},
+				{
+					businessId: 2,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/wip3.png",
+				},
+				{
+					businessId: 2,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/wip4.png",
+				},
+				{
+					businessId: 3,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck.png",
+				},
+				{
+					businessId: 3,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck2.png",
+				},
+				{
+					businessId: 3,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck3.png",
+				},
+				{
+					businessId: 3,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
+				},
+				{
+					businessId: 4,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck.png",
+				},
+				{
+					businessId: 4,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck2.png",
+				},
+				{
+					businessId: 4,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck3.png",
+				},
+				{
+					businessId: 4,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
+				},
+				{
+					businessId: 5,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck.png",
+				},
+				{
+					businessId: 5,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck2.png",
+				},
+				{
+					businessId: 5,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck3.png",
+				},
+				{
+					businessId: 5,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
+				},
+				{
+					businessId: 6,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck.png",
+				},
+				{
+					businessId: 6,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck2.png",
+				},
+				{
+					businessId: 6,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck3.png",
+				},
+				{
+					businessId: 6,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
+				},
+				{
+					businessId: 7,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck.png",
+				},
+				{
+					businessId: 7,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck2.png",
+				},
+				{
+					businessId: 7,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck3.png",
+				},
+				{
+					businessId: 7,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
+				},
+				{
+					businessId: 8,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck.png",
+				},
+				{
+					businessId: 8,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck2.png",
+				},
+				{
+					businessId: 8,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck3.png",
+				},
+				{
+					businessId: 8,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
+				},
+				{
+					businessId: 9,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck.png",
+				},
+				{
+					businessId: 9,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck2.png",
+				},
+				{
+					businessId: 9,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck3.png",
+				},
+				{
+					businessId: 9,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
+				},
+				{
+					businessId: 10,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck.png",
+				},
+				{
+					businessId: 10,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck2.png",
+				},
+				{
+					businessId: 10,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck3.png",
+				},
+				{
+					businessId: 10,
+					imageURL:
+						"https://grumble.s3.us-west-1.amazonaws.com/duck4.png",
+				},
 			],
 			{}
 		);
